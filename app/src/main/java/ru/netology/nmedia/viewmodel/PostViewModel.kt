2f2269156e82a2d398ba2c05fdf1a34372b34b28
Aleksey Mladinskiy <alekseymladinskiy@gmail.com>
2025-08-15 19:07:14 +0300

    addedFlow

---
+++ app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -0,0 +1,155 @@
+package ru.netology.nmedia.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import ru.netology.nmedia.api.PostsApi
+import ru.netology.nmedia.db.AppDb
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.model.FeedModel
+import ru.netology.nmedia.model.FeedModelState
+import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryImpl
+import ru.netology.nmedia.util.SingleLiveEvent
+
+private val empty = Post(
+    id = 0,
+    content = "",
+    author = "",
+    authorAvatar = "",
+    likedByMe = false,
+    likes = 0,
+    published = ""
+)
+
+class PostViewModel(application: Application) : AndroidViewModel(application) {
+    // упрощённый вариант
+    private val repository: PostRepository =
+        PostRepositoryImpl(AppDb.getInstance(context = application).postDao())
+
+    val data: LiveData<FeedModel> = repository.data
+        .map { list: List<Post> -> FeedModel(list, list.isEmpty()) } //(::FeedModel)
+        .catch { it.printStackTrace() }
+        .asLiveData(Dispatchers.Default)
+
+    private val _dataState = MutableLiveData<FeedModelState>()
+    val dataState: LiveData<FeedModelState>
+        get() = _dataState
+
+    val newerCount: LiveData<Int> = data.switchMap {
+        repository.getNewerCount(it.posts.firstOrNull()?.id ?: 0L)
+            .catch { e -> e.printStackTrace() }
+            .asLiveData(Dispatchers.Default)
+    }
+
+    private val edited = MutableLiveData(empty)
+    private val _postCreated = SingleLiveEvent<Unit>()
+    val postCreated: LiveData<Unit>
+        get() = _postCreated
+
+    init {
+        loadPosts()
+    }
+
+    fun loadPosts() = viewModelScope.launch {
+        try {
+            _dataState.value = FeedModelState(loading = true)
+            repository.getAll()
+            _dataState.value = FeedModelState()
+        } catch (e: Exception) {
+            _dataState.value = FeedModelState(error = true)
+        }
+    }
+
+    fun refreshPosts() = viewModelScope.launch {
+        try {
+            _dataState.value = FeedModelState(refreshing = true)
+            repository.getAll()
+            _dataState.value = FeedModelState()
+        } catch (e: Exception) {
+            _dataState.value = FeedModelState(error = true)
+        }
+    }
+
+    fun save() {
+        edited.value?.let {
+            _postCreated.value = Unit
+            viewModelScope.launch {
+                try {
+                    repository.save(it)
+                    _dataState.value = FeedModelState()
+                } catch (e: Exception) {
+                    _dataState.value = FeedModelState(error = true)
+                }
+            }
+        }
+        edited.value = empty
+    }
+
+    fun edit(post: Post) {
+        edited.value = post
+    }
+
+    fun changeContent(content: String) {
+        val text = content.trim()
+        if (edited.value?.content == text) {
+            return
+        }
+        edited.value = edited.value?.copy(content = text)
+    }
+
+    fun likeById(id: Long) {
+        viewModelScope.launch {
+            try {
+                _dataState.value = FeedModelState(loading = true)
+                repository.likeById(id)
+                _dataState.value = FeedModelState()
+
+            } catch (e: Exception) {
+                _dataState.value = FeedModelState(error = true)
+            }
+        }
+    }
+
+    fun removeById(id: Long) {
+        viewModelScope.launch {
+            try {
+                _dataState.value = FeedModelState(loading = true)
+                repository.removeById(id)
+                _dataState.value = FeedModelState()
+
+            } catch (e: Exception) {
+                _dataState.value = FeedModelState(error = true)
+            }
+        }
+    }
+
+    private val _newPostsCount = MutableLiveData(0)
+    val newPostsCount: LiveData<Int> = _newPostsCount
+
+    private val _isNewPostsButtonVisible = MutableLiveData(false)
+    val isNewPostsButtonVisible: LiveData<Boolean> = _isNewPostsButtonVisible
+
+    init {
+        viewModelScope.launch {
+            repository.getNewerCount(0)
+                .flowOn(Dispatchers.Default)
+                .catch { e -> e.printStackTrace() }
+                .collectLatest { count ->
+                    _newPostsCount.value = count
+                    _isNewPostsButtonVisible.value = count > 0
+                }
+        }
+    }
+    fun showNewPosts() {
+        _isNewPostsButtonVisible.value = false
+    }
+
+}
